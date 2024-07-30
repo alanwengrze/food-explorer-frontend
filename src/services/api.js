fbe1462b724bdbdf48fdb1c819e5d4027675908e
alan <alanwengrzee@gmail.com>
2024-07-29 22:27:05 -0300

    :bricks: ci: url changed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://food-explorer-api-3bk1.onrender.com",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://food-explorer-api-3bk1.onrender.com",
   
 })
